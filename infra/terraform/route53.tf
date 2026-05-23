a49f30239d4b4cc4023c01c63b70407c3b0c50ef
Colby Smith <colbysmith99@hotmail.com>
2026-05-23 15:36:34 +0100

    Update Route53.tf
    - added dns records to the hosted zone for ipv4 & ipv6.
    - added cloudfront distributions to the hosted zone.

--- infra/terraform/route53.tf
+++ infra/terraform/route53.tf
@@ -5,3 +5,51 @@ resource "aws_route53_zone" "main" {
     Name = "${var.project_name}-route53-zone-${var.environment_suffix}"
   }
 }
+
+resource "aws_route53_record" "root_a" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.website.domain_name
+    zone_id                = aws_cloudfront_distribution.website.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "root_aaaa" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = var.domain_name
+  type    = "AAAA"
+
+  alias {
+    name                   = aws_cloudfront_distribution.website.domain_name
+    zone_id                = aws_cloudfront_distribution.website.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "www_a" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = var.www_domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.website.domain_name
+    zone_id                = aws_cloudfront_distribution.website.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "www_aaaa" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = var.www_domain_name
+  type    = "AAAA"
+
+  alias {
+    name                   = aws_cloudfront_distribution.website.domain_name
+    zone_id                = aws_cloudfront_distribution.website.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
